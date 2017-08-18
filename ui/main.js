6b45320fd39ffbce840cc026ec88840a875d704a
Zahid2888 <30659855+Zahid2888@users.noreply.github.com>
2017-08-18 19:15:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,4 +17,8 @@ button.onclick = function() {
     request.send(null);
 };
 
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit');
+
 
