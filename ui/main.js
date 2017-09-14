357d845c79d92653aebc891ca917d747e1906b00
Zahid2888 <30659855+Zahid2888@users.noreply.github.com>
2017-09-14 19:28:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,23 +5,22 @@ submit.onclick = function() {
     request.onreadystatechange = function() {
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200) {
-                var names = request.responseText;
-                names = JSON.parse(names);
-                var list = '';
-                for(var i=0;i<names.length;i++){
-                    list += '<li>' + names[i] + '</li>';
-                }
-            var lu = document.getElementById('nameList');
-            console.log(lu);
-            lu.innerHTML = list;
+                console.log('user logged in');
+                alert('logged in succesfully');
+            } else if (request.status === 403) {
+                alert('username/password is incorrect');
+            } else if (request.status === 500) {
+                alert('something went wrong on the server');
             }
-        }
+            }
+        };
     };
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
     console.log(username);
     console.log(password);
-    request.open('POST','http://zahid2888.imad.hasura-app.io/submit-name?name=' + name,true);
+    request.open('POST','http://zahid2888.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username: username, password: password}));
 };
 
