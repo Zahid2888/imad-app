9a97b8df5b87ef1adbc39abb014092bbb55e2527
Zahid2888 <30659855+Zahid2888@users.noreply.github.com>
2017-09-14 19:29:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,7 +14,7 @@ submit.onclick = function() {
             }
             }
         };
-    };
+    
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
     console.log(username);
